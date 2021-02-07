0830b3c1203990996ed5bcade901e27bb3a32f7f
amaralc <amaral.calil@gmail.com>
2021-02-07 11:51:34 -0300

    FEATURE / Create first chart

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  Highcharts.chart('container', {})
+  Highcharts.chart('container', {
+    chart: {
+      type: 'line',
+      zoomType: 'x',
+    },
+    title: {
+      text: 'First chart'
+    },
+    yAxis: {
+      title: {
+        text: 'Fruits Eaten',
+      }
+    },
+    xAxis: {
+      title: {text: 'Fruits'},
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    series: [
+      {
+        name: 'John',
+        data: [1,2,3]
+      }
+      ,
+      {
+        name: 'Jane',
+        data: [2, 4, 8]
+      }
+    ]
+  })
 })
